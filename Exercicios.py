c1fedbaeb2833dc2c729b348de9c0a34f347ad0f
Vitor Cordeiro de Souza <vitorcorddesouza@gmail.com>
2024-02-28 09:40:45 -0300

    fabricio

---
+++ Exercicios.py
@@ -0,0 +1,64 @@
+"""
+valor1 = float(input("Digite um valor: "))
+valor2 = float(input("Digite outro valor: "))
+if valor1 > valor2:
+    print(f"{valor1} maior que {valor2}")
+else:
+    print(f"{valor2} maior que {valor1}")
+"""
+
+"""
+idade = int(input("Digite seu ano de nascimento: "))
+if idade <= 2008:
+    print("Voce pode votar")
+else:
+    print("Voce nao pode votar")
+"""
+
+"""
+senha = int(input("digite uma senha: "))
+if senha == 1234:
+    print("acesso permitido")
+else:
+    print("acesso negado")
+"""
+
+"""
+valor = float
+qtd = float(input("digite quantas maças voce comprou:"))
+if qtd < 12:
+   valor = qtd*0.3
+   print(f"O valor é: {valor}")
+else:
+   valor = qtd*0.25
+   print(f"O valor é: {valor}")
+"""
+
+"""
+valor1 = int(input("Digite o primeiro numero: "))
+valor2 = int(input("Digite o segundo numero: "))
+valor3 = int(input("Digite o terceiro numero: "))
+if valor1 < valor2 < valor3:
+    print(valor1, valor2, valor3)
+elif valor1 < valor3 < valor2:
+    print(valor1, valor3, valor2)
+elif valor2 < valor1 < valor3:
+    print(valor2, valor1, valor3)
+elif valor2 < valor3 < valor1:
+    print(valor2, valor3, valor1)
+elif valor3 < valor2 < valor1:
+    print(valor3, valor2, valor1)
+else:
+    print( valor3, valor1, valor2)        
+"""
+"""
+altura = float(input("Digite sua altura em metros: "))
+sexo = int(input("Digite 1 para feminino e 2 para masculino: "))
+pesoIdeal = float
+if sexo == 1:
+    pesoIdeal = (62.1 * altura) - 44.7
+    print(f"Seu peso ideal é {pesoIdeal}")
+else:
+   pesoIdeal = (72.7 * altura) - 58
+   print(f"Seu peso ideal é {pesoIdeal}")
+"""
